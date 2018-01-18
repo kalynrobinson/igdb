d2aa2ce7aff6ab11242144ebd25e89d862c2418a
Michael Vasallo <mjrvasallo@gmail.com>
2018-01-18 01:03:00 -0600

    Fix model specs
    * Handle testing in api_resource_spec
    * Test resource object types in a loop
    * Configure vcr to filter API key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,5 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
+  config.filter_sensitive_data('<IGDB_KEY>') { ENV['IGDB_KEY'] }
 end
